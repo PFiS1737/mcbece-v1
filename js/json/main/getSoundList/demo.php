9f79dd176d699f7c921a9e990c277c626efbfd36
PFiS1737 <3165823482@qq.com>
2021-07-13 11:54:56 +0800

    update demo.php

--- js/json/main/getSoundList/demo.php
+++ js/json/main/getSoundList/demo.php
@@ -1,14 +1,19 @@
-// 获取当前文件的上级目录
-$con = dirname(__FILE__);
-// 扫描$con目录下的所有文件
-$filename = scandir($con);
-// 定义一个数组接收文件名
-$conname = array();
-foreach($filename as $k=>$v){
-    // 跳过两个特殊目录   continue跳出循环
-    if($v=="." || $v==".."){continue;}
-    //截取文件名，我只需要文件名不需要后缀;然后存入数组。如果你是需要后缀直接$v即可
-    $conname[] = $v;
-}
+<?php
 
-echo $conname
+    header('Content-Type: application/json; charset=UTF-8');
+
+
+$con = dirname(__FILE__);
+$filename = scandir($con);
+$conname = array();
+foreach ($filename as $k => $v) {
+    if($v == "." || $v == "..") {
+        continue;
+    }
+    $conname[] = $v;
+};
+echo $conname;
+
+    echo "{'demo': 'demo'}";
+
+?>
